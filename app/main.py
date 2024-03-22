bf0d8370398c2fdef1633a1de6bc4f19db7d1538
lharo <haroomg@gmail.com>
2024-03-22 14:15:31 -0400

    holis de nuevo

--- app/main.py
+++ app/main.py
@@ -1,18 +1,41 @@
+from .tools.classes import Request
+from fastapi import HTTPException
 from .match import match_img
 from fastapi import FastAPI
-from .request import load
 from .shema import *
 
 
 app = FastAPI()
 
 #3
-# @app.post("/request/load/")
-# def load_request(request: Load_reques) -> dict:
+@app.post("/request/load/")
+def load_request(request: Load_reques) -> dict:
     
-#     request_id: str = request.request_id
-    
-#     return load(request_id)
+    request_id = Load_reques.request_id
+    request_ = Request(request_id)
+
+    if request_.exists_request == True:
+
+        if request_.exists_schema == False:
+
+            request.create_schema()
+
+        request.load_data()
+
+        msm = {
+            "msm": "La data acaba de ser subida.",
+            "request_id": request_.request_id,
+            "schema_name": request_.schema_name
+        }
+
+        return 
+
+    else:
+        msm = {
+            "msm": "El request no existe o esta mal escrito",
+            "request_id": request_id
+        }
+        raise HTTPException(status_code=409, detail=msm)
 
 
 @app.post("/match/img")
@@ -20,9 +43,7 @@ def match_images(request: Match_img) -> dict:
     
     request = {
         "request_id": request.request_id,
-        "input": request.input,
-        "s3_path_img_origin": request.s3_path_img_origin,
-        "s3_path_img_alternative": request.s3_path_img_alternative
+        "input": request.input
     }
     
     return match_img(**request)
